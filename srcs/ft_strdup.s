4d9e635d9e6f2e7769d65d1ee71dfcda5514e102
Raúl Cabezas González <raul.29cg@gmail.com>
2021-02-04 14:23:09 +0100

    Haciendo ft_strdup

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -2,7 +2,13 @@ section .text
     global _ft_strdup
     extern _malloc
     extern _ft_strlen
+    extern _ft_strcpy
 
 _ft_strdup :
-    mov rax, rdi
+    call _ft_strlen
+    mov rdi, rax
+    call _malloc
+    mov rsi, rdi
+    mov rdi, rax
+    call _ft_strcpy
     ret
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/17 18:01:45 by rcabezas          #+#    #+#             */
-/*   Updated: 2021/02/04 12:47:19 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/02/04 13:08:15 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@
 int main(void)
 {
     const char *s = "hola";
-    const char *x = "hola";
-    int y;
-    int z;
-//    char *a;
-//    char *b;
+//    const char *x = "hola";
+//    int y;
+//    int z;
+    char *a;
+    char *b;
 
-    /*a = ft_strdup(s);
+    a = ft_strdup(s);
     b = strdup(s);
     printf("RETURN : %s\n", a);
-    printf("ORIGINAL : %s\n", b);*/
-    y = ft_strcmp(s, x);
+    printf("ORIGINAL : %s\n", b);
+    /*y = ft_strcmp(s, x);
     z = strcmp(s, x);
     printf("RETURN : %d\n", y);
-    printf("ORIGINAL : %d\n", z);
+    printf("ORIGINAL : %d\n", z);*/
 }
