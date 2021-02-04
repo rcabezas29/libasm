1dfcfb5c9b3b15361ce5fc3026424557bcecd63d
Raul Cabezas gonzalez <rcabezas@c1r2s2.42madrid.com>
2021-02-04 11:38:29 +0100

    Hecho ft_write

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,7 +6,7 @@
 /*   By: rcabezas <rcabezas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 10:36:39 by rcabezas          #+#    #+#             */
-/*   Updated: 2020/12/23 10:47:26 by rcabezas         ###   ########.fr       */
+/*   Updated: 2021/02/04 11:21:31 by rcabezas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@
 size_t  ft_strlen(const char *s);
 char    *ft_strcpy(char *dst, const char *src);
 int     ft_strcmp(const char *s1, const char *s2);
-ssize_t ft_write(int fildes, const void *buf, size_t nbyte, off_t offset);
+ssize_t ft_write(int fildes, const void *buf, size_t nbyte);
 ssize_t ft_read(int fildes, void *buf, size_t nbyte);
 #endif
